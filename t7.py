4fe5f175b67e5dd811cc8026f9892d1218232cf2
YanLin <chriszt@126.com>
2021-02-26 02:40:49 +0800

    modify t7.py

--- t7.py
+++ t7.py
@@ -1,13 +1,25 @@
 import numpy as np
 
-t1 = np.arange(12).reshape(2, 6)
+# t1 = np.arange(12, dtype="float").reshape(3, 4)
+# print(t1)
+#
+# t2 = np.zeros((3, 4), dtype="bool")
+# t2[0, 1] = True
+# t2[1, 0] = True
+# t2[1, 2] = True
+# print(t2)
+#
+# t3 = t1 * t2
+# print(t3)
+
+t1 = np.arange(1, 13).reshape(3, 4)
 print("t1:\n", t1)
 
-t2 = np.arange(12, 24).reshape(2, 6)
+t2 = np.array([[True, False, False, False],
+               [True, True, False, False],
+               [False, False, False, True]])
 print("t2:\n", t2)
 
-t3 = np.vstack((t1, t2))
+t3 = t1[t2]
 print("t3:\n", t3)
 
-t4 = np.hstack((t1, t2))
-print("t4:\n", t4)
