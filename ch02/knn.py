f3ed6a749a2dd848c9351a2482fceb753b0fe78b
YanLin <chriszt@126.com>
2021-03-03 17:32:07 +0800

    start KNN(ch02) day3

--- ch02/knn.py
+++ ch02/knn.py
@@ -26,15 +26,7 @@ def classify(inX, ds, dsLabs, k):
     return sortedClsCount[0][0]
 2
 
-def createDataSet():
-    _grp = np.array([[1.0, 1.1],
-                     [1.0, 1.0],
-                     [0.0, 0.0],
-                     [0.0, 0.1]])
-    _labs = ["A", "A", "B", "B"]
-    # print("Group:\n", _grp)
-    # print("Labels:\n", _labs)
-    return _grp, _labs
+
 
 
 def file2mat(filename):
@@ -61,6 +53,76 @@ def file2mat(filename):
         return retMat, clsLabVec
 
 
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    deltas = maxVals - minVals
+    normDataSet = np.zeros(dataSet.shape)
+    rowNum = dataSet.shape[0]
+    normDataSet = dataSet - np.tile(minVals, (rowNum, 1))
+    normDataSet = normDataSet / np.tile(deltas, (rowNum, 1))
+    return normDataSet, deltas, minVals
+
+
+def createDataSet():
+    _grp = np.array([[1.0, 1.1],
+                     [1.0, 1.0],
+                     [0.0, 0.0],
+                     [0.0, 0.1]])
+    _labs = ["A", "A", "B", "B"]
+    # print("Group:\n", _grp)
+    # print("Labels:\n", _labs)
+    return _grp, _labs
+
+
+def datClsTest():
+    sepRation = 0.1
+    datDataMat, datLabels = file2mat("./datingTestSet2.txt")
+    normMat, deltas, minVals = autoNorm(datDataMat)
+    rowNum = normMat.shape[0]
+    testVecNum = int(rowNum * sepRation)
+    errCount = 0.0
+    for i in range(testVecNum):
+        clsRes = classify(normMat[i, :], normMat[testVecNum:rowNum, :], datLabels[testVecNum:rowNum], 3)
+        print("The classifier came back with: %d, the real answer is: %d" % (clsRes, datLabels[i]))
+        if clsRes != datLabels[i]:
+            errCount += 1.0
+    print("the total error rate is: %f" % (errCount / float(testVecNum)))
+    print(errCount)
+
+
+def clsPerson():
+    resLabels = ["Not at all", "Small doses", " Large doses"]
+    mile = float(input("frequent flier miles earned per year: "))
+    game = float(input("percentage of time spent playing video games: "))
+    ice = float(input("liters of ice cream consumed per year: "))
+    datDataMat, datLabels = file2mat("./datingTestSet2.txt")
+    normMat, deltas, minVals = autoNorm(datDataMat)
+    newArr = np.array([mile, game, ice])
+    # print(newArr)
+    newArrNorm = (newArr - minVals) / deltas
+    # print(newArrNorm)
+    clsRes = classify(newArrNorm, datDataMat, datLabels, 3)
+    print("You will probably like this person: ", resLabels[clsRes - 1])
+
+
+def img2Vec(filename):
+    retVec = np.zeros((1, 1024))
+    with open(filename) as fp:
+        for i in range(32):
+            curLine = fp.readline().strip()
+            # print(curLine)
+            for j in range(32):
+                retVec[0, (i * 32) + j] = curLine[j]
+    return retVec
+
+
+def handWriteTest():
+    hwLables = []
+
+    pass
+
+
 if __name__ == '__main__':
     ########## Test 1 ##########
     # group, labels = createDataSet()
@@ -70,24 +132,41 @@ if __name__ == '__main__':
 
     ########## Test 2 ##########
     # file2mat("/dev/random")
-    datDataMat, datLabels = file2mat("./datingTestSet2.txt")
+    # datDataMat, datLabels = file2mat("./datingTestSet2.txt")
+    # normMat, deltas, minVals = autoNorm(datDataMat)
     # print("datDataMat:\n", datDataMat)
     # print("datLabels:\n", datLabels)
-    print(datDataMat[1:3, 1], datDataMat[1:3, 2])
-    plt.scatter(datDataMat[1:3, 1], datDataMat[1:3, 2])
-    plt.scatter(datDataMat[3:5, 1], datDataMat[3:5, 2])
+    # print(datDataMat[:, 1], datDataMat[:, 2])
+    # print(np.array(datLabels))
+    # plt.scatter(datDataMat[:, 1], datDataMat[:, 2], 6 ** 2)
+    # print(plt.rcParams["lines.markersize"])
+    # plt.scatter(datDataMat[:, 1], datDataMat[:, 2], datLabels)
+    # plt.grid()
+    # plt.show()
 
-    plt.grid()
-    plt.show()
+    ########## Test 3 ##########
+    # datClsTest()
 
+    ########## Test 4 ##########
+    # clsPerson()
+
+    ########## Test 5 ##########
+    # img2Vec("trainingDigits/0_0.txt")
+
+    ########## Test 6 ##########
+    handWriteTest()
 
     ########## Test X ##########
-    # arr1 = np.arange(4).reshape((2, 2))
-    # print(arr1)
-    # # arr1 = np.tile(arr1, 2)
-    # print(np.tile(arr1, 2))
-    # # arr1 = np.tile(arr1, (1, 2))
-    # print(np.tile(arr1, (1, 2)))
+    # t0 = np.arange(4).reshape((2, 2))
+    # print(t0)
+    # t1 = np.tile(t0, 2)
+    # print(t1)
+    # t2 = np.tile(t0, (1, 2))
+    # print(t2)
+    # t3 = np.tile(t0, (2, 1))
+    # print(t3)
+    # t4 = np.tile(t0, (2, 2))
+    # print(t4)
 
     # fruits = {"apple": 3, "banana": 2, "pear": 5, "orange": 1}
     # print(fruits)
